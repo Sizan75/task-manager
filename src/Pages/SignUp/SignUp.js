2966004d4285dd769ea12a3d4b91ab9c75ea1ddf
Sizan75 <sakiuzzaman26@gmail.com>
2022-12-29 04:39:22 +0600

    signup done

---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,116 @@
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { FcGoogle } from "react-icons/fc";
+import toast from 'react-hot-toast';
+import { GoogleAuthProvider } from 'firebase/auth';
+import { AuthContext } from '../../Context/AuthProvider';
+
+const SignUp = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [signUpError, setSignUPError] = useState('')
+    const { createUser, updateUser, singInGoogle } = useContext(AuthContext);
+    const googleProvider = new GoogleAuthProvider()
+
+
+    const navigate = useNavigate()
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/'
+
+
+    const handleSignUp = (data) => {
+        setSignUPError('');
+        createUser(data.email, data.password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                toast('User Created Successfully.')
+
+                const userInfo = {
+                    displayName: data.name,
+                    email: data.email,
+                }
+                updateUser(userInfo)
+                    .then(() => {
+                        saveUser(data.name, data.email)
+
+                    })
+                    .catch(err => console.log(err));
+            })
+            .catch(error => {
+                console.log(error)
+                setSignUPError(error.message)
+            });
+    }
+
+    const saveUser = (name,  email) => {
+
+        const user = {
+            displayName: name,
+            email: email,
+        }
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        }).then(res => res.json())
+            .then(data => {
+
+            })
+    }
+
+    const handleGoogleSignIn = () => {
+        
+        singInGoogle(googleProvider)
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+                saveUser(user.displayName, user.email)
+
+            })
+            .catch(error => console.error(error))
+    }
+
+    return (
+        <div className='h-[800px] flex justify-center items-center'>
+            <div className='w-96 p-7'>
+                <h2 className='text-xl text-center'>Sign Up</h2>
+                <form onSubmit={handleSubmit(handleSignUp)}>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Name</span></label>
+                        <input type="text" {...register("name", {
+                            required: "Name is Required"
+                        })} className="input input-bordered input-success w-full max-w-xs" />
+                        {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Email</span></label>
+                        <input type="email" {...register("email", {
+                            required: true
+                        })} className="input input-bordered input-success w-full max-w-xs" />
+                        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Password</span></label>
+                        <input type="password" {...register("password", {
+                            required: "Password is required",
+                            minLength: { value: 6, message: "Password must be 6 characters long" },
+                            pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: 'Password must have uppercase, number and special characters' }
+                        })} className="input input-bordered input-success w-full max-w-xs" />
+                        {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
+                    </div>
+                    <input className='bg-blue-500 w-full mt-4' value="Sign Up" type="submit" />
+                    {signUpError && <p className='text-red-600'>{signUpError}</p>}
+                </form>
+                <p>Already have an account <Link className='text-secondary' to="/login">Please Login</Link></p>
+                <div className="divider">OR</div>
+                <button onClick={handleGoogleSignIn} className="px-8 py-3 font-semibold border rounded dark:border-gray-100 dark:text-gray-100"><FcGoogle></FcGoogle>  Google Sign In</button>
+
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import AddTask from "../Pages/AddTask.js/AddTask";
 import Home from "../Pages/Home/Home";
+import SignUp from "../Pages/SignUp/SignUp";
 
 export const router = createBrowserRouter([
 
@@ -19,6 +20,10 @@ export const router = createBrowserRouter([
             {
                 path: '/addtask',
                 element: <AddTask></AddTask>
+            },
+            {
+                path: '/signup',
+                element: <SignUp></SignUp>
             }           
         ]
 }])
